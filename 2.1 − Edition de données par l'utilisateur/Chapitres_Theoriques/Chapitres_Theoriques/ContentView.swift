2b866c8861bcf64c55b14bc0de05c381f1314346
Maxime Britto <maxime@purplegiraffe.fr>
2020-07-07 14:47:11 +0200

    21_1_Utiliser_SF_Symbols

---
+++ 2.1 − Edition de données par l'utilisateur/Chapitres_Theoriques/Chapitres_Theoriques/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Chapitres_Theoriques
+//
+//  Created by Maxime Britto on 07/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image(systemName: "pencil.tip.crop.circle.badge.plus").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
