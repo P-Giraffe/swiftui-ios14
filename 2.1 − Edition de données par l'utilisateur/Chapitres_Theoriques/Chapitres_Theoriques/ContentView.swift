05e8b8da9fdf44ecb99f3424a8cae242aa148885
Maxime Britto <maxime@purplegiraffe.fr>
2020-07-07 17:24:02 +0200

    21_6_Saisie_de_texte_par_lutilisateur

--- 2.1 − Edition de données par l'utilisateur/Chapitres_Theoriques/Chapitres_Theoriques/ContentView.swift
+++ 2.1 − Edition de données par l'utilisateur/Chapitres_Theoriques/Chapitres_Theoriques/ContentView.swift
@@ -8,15 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var orderName = ""
     @State var pencilCount = 0
     @State var penCount = 0
     var body: some View {
         VStack {
+            TextField("Nom de la commande", text: $orderName)
             PencilStack(title: "Ajouter un stylo", clickCount: $penCount)
                 .padding()
             PencilStack(title: "Ajouter un crayon", clickCount: $pencilCount)
                 .padding()
-            Text("Nombre total d'éléments commandés : \(pencilCount + penCount)")
+            Text("Nombre total d'éléments commandés pour \(orderName) : \(pencilCount + penCount)")
         }
     }
 }
