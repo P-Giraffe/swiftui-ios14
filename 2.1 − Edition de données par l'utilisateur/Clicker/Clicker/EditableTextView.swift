12501957a8dc881dea5eb43e2b30aed7f21b0e33
Maxime Britto <maxime@purplegiraffe.fr>
2020-07-07 13:06:21 +0200

    21_4_Creer_le_mode_edition_du_pseudo

--- 2.1 − Edition de données par l'utilisateur/Clicker/Clicker/EditableTextView.swift
+++ 2.1 − Edition de données par l'utilisateur/Clicker/Clicker/EditableTextView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct EditableTextView: View {
+    @State var isEditing = false
     @Binding var editedText:String
     var body: some View {
         HStack {
-            Text(editedText)
+            if isEditing == true {
+                TextField("Pseudo", text: $editedText)
+            } else {
+                Text(editedText)
+            }
             Image(systemName: "pencil.circle")
+                .onTapGesture {
+                    isEditing.toggle()
+                }
         }
     }
 }
