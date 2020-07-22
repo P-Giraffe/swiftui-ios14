4ba00d33def5ab9f30a0e7d6c5bfd06110351353
Maxime Britto <maxime@purplegiraffe.fr>
2020-07-23 01:45:22 +0200

    51_4_Configuration_de_CoreData

--- 5_Bases_de_donnees/Tasks/Tasks/Data/Storage/CoreDataStorage.swift
+++ 5_Bases_de_donnees/Tasks/Tasks/Data/Storage/CoreDataStorage.swift
@@ -6,8 +6,24 @@
 //
 
 import Foundation
+import CoreData
 
 class CoreDataStorage {
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+            let container = NSPersistentContainer(name: "Tasks")
+            container.loadPersistentStores { description, error in
+                if let error = error {
+                    fatalError("Unable to load persistent stores: \(error)")
+                }
+            }
+            return container
+        }()
+    
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
     func fetchTaskList() -> [Task] {
         return []
     }
