05c46aaf86a7f15e61fa4cc0e5d87149ef60f316
Maxime Britto <maxime@purplegiraffe.fr>
2020-12-11 13:14:42 +0100

    91_4_NavigationLink_declenche_automatiquement

--- 9_navigation/navigation/navigation/Accueil.swift
+++ 9_navigation/navigation/navigation/Accueil.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct Accueil: View {
+    @State var motDePasseSaisi = ""
+    @State var motDePasseValide = false
     var body: some View {
         VStack {
             Text("Bienvenue dans votre app")
@@ -19,10 +21,25 @@ struct Accueil: View {
                     Image(systemName: "star")
                 }
             }
-                
+            SecureField("Mot de passe", text: $motDePasseSaisi)
+            Button("Aller à l'écran secret", action: boutonSecretTouché)
+            NavigationLink(
+                destination: Ecran2(),
+                isActive: $motDePasseValide,
+                label: {
+                    EmptyView()
+                })
         }.padding()
         .navigationTitle("Accueil")
         
+    }
+    
+    func boutonSecretTouché() {
+        if motDePasseSaisi == "J'aime Purple Giraffe" {
+            motDePasseValide = true
+        } else {
+            motDePasseValide = false
+        }
     }
 }
 
